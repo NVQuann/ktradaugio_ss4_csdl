af973334e403b42f4e583b9f3aa01ecd298e0c78
NVQuann <nguyenvanquan5820007@gmail.com>
2026-04-17 07:37:53 +0700

    Add files via upload

---
+++ ktradaugio.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE shopManager_db;
+
+use shopManager_db;
+
+CREATE TABLE categories( 
+	categoryId INT PRIMARY KEY,
+    categoryName VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE products(
+	productId INT,
+    productName VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) CHECK(price > 0),
+    stock INT CHECK(stock > 0),
+    categoryId INT,
+    
+    PRIMARY KEY(productId, categoryId),
+    
+    FOREIGN KEY (categoryId) REFERENCES categories(categoryId)
+);
+
+-- Thêm dữ liệu
+INSERT INTO categories
+VALUES 
+(1, 'Điện tử'),
+(2, 'Thời trang'); 
+
+INSERT INTO products
+VALUES 
+(1, 'iPhone 15', 25000000, 10, 1),
+(2, 'Samsung S23', 20000000, 5, 1),
+(3, ' Áo sơ mi nam', 500000, 50, 2),
+(4, 'Giày thể thao', 12000000, 20, 2);
+
+-- Cập nhật
+UPDATE products
+SET price = 26000000
+WHERE productId = 1; 
+
+UPDATE products
+SET price = price + 10
+WHERE productId;
+
+
+
+-- Xóa 
+DELETE FROM products
+WHERE productId = 4 AND categoryId = 2;
+
+DELETE FROM products
+WHERE price < 1000000;
+
+-- Lệnh SELECT
+SELECT * FROM products; 
+
+SELECT * FROM products
+WHERE stock > 15;
